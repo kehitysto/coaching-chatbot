7bb790528d442f7d7b8ab8d85cc3da2be9d2bfca
Henry Carlson <henry.carlson@gmail.com>
2017-01-28 14:42:55 +0200

    Add service for handling Wit.ai sessions

--- src/wit-ai/handler.js
+++ src/wit-ai/handler.js
@@ -2,9 +2,13 @@
 
 require('../lib/envVars').config();
 
+import Sessions from './sessions.service';
+
+
 module.exports.handler = (event, context, cb) => {
-  if (!process.env.WIT_AI_TOKEN) {
-    return cb('No WIT_AI_TOKEN defined');
-  }
-  return cb('No SNS event');
+    if (!process.env.WIT_AI_TOKEN) {
+        return cb('No WIT_AI_TOKEN defined');
+    }
+
+    return cb('No SNS event');
 };
