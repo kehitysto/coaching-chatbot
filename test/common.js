8038dba92a8051240331eacd5b881e58dda9300f
Henry Carlson <henry.carlson@gmail.com>
2017-02-01 21:44:35 +0200

    Improve UT framework, add first real cases

---
+++ test/common.js
@@ -0,0 +1,12 @@
+/**
+* Common test fixture for all test cases
+*/
+
+global.chai = require('chai');
+global.expect = require('chai').expect;
+
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
