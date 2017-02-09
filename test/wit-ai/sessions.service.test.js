a37b354731095c0b42fe2589d7ee1a649742dbe7
Atte Keltanen <atke@cs.helsinki.fi>
2017-02-09 11:41:30 +0200

    fix tests

--- test/wit-ai/sessions.service.test.js
+++ test/wit-ai/sessions.service.test.js
@@ -3,98 +3,119 @@ import sinon from 'sinon';
 import AWS from 'aws-sdk';
 import Sessions from '../../src/wit-ai/sessions.service';
 
-
 describe('Sessions service', function() {
+  before(function() {
+    this.db = sinon.stub(AWS.DynamoDB, 'DocumentClient');
+    this.sessions = new Sessions();
+  });
+
+  after(function() {
+    this.db.restore();
+  });
+
+  describe('#read()', function() {
     before(function() {
-        this.db = sinon.stub(AWS.DynamoDB, 'DocumentClient');
-        this.sessions = new Sessions();
+      this.db.prototype.get = sinon.stub()
+        .callsArgWith(
+          1,
+          null, {
+            Item: {
+              id: 'SESSION_ID',
+              context: {
+                key: 'value'
+              }
+            }
+          }
+        );
     });
 
-    after(function() {
-        this.db.restore();
+    it('should return a Promise', function() {
+      const ret = this.sessions.read('SESSION_ID');
+
+      expect(ret)
+        .to.be.a('Promise');
+
+      return ret;
     });
 
-    describe('#read()', function() {
-        before(function() {
-            this.db.prototype.get = sinon.stub().callsArgWith(
-                1,
-                null,
-                {
-                    id: 'SESSION_ID',
-                    context: {
-                        key: 'value'
-                    }
-                }
+    it('should request the session context from DynamoDB', function() {
+      return expect(
+          this.sessions.read('SESSION_ID')
+        )
+        .to.eventually.be.fulfilled.then(() => {
+          return expect(this.db.prototype.get)
+            .to.have.been.calledWith(
+              sinon.match({
+                Key: {
+                  id: 'SESSION_ID'
+                },
+                TableName: sinon.match.string
+              }),
+              sinon.match.func
             );
         });
+    });
 
-        it('should return a Promise', function() {
-            const ret = this.sessions.read('SESSION_ID');
-
-            expect(ret).to.be.a('Promise');
-
-            return expect(ret).to.eventually.be.fulfilled;
+    it('should resolve with context from DynamoDB', function() {
+      return expect(
+          this.sessions.read('SESSION_ID')
+        )
+        .to.become({
+          key: 'value'
         });
+    });
+  });
 
-        it('should request the session context from DynamoDB', function() {
-            return expect(
-                this.sessions.read('SESSION_ID')
-            ).to.eventually.be.fulfilled.then(() => {
-                return expect(this.db.prototype.get).to.have.been.calledWith(
-                    sinon.match({
-                        Key: { id: 'SESSION_ID' },
-                        TableName: sinon.match.string
-                    }),
-                    sinon.match.func
-                );
-            });
-        });
+  describe('#write()', function() {
+    before(function() {
+      this.db.prototype.put = sinon.stub()
+        .callsArg(1);
+    });
 
-        it('should resolve with context from DynamoDB', function() {
-            return expect(
-                this.sessions.read('SESSION_ID')
-            ).to.become({
-                key: 'value'
-            });
+    it('should return a Promise', function() {
+      const ret = this.sessions.write('SESSION_ID', {
+        key: 'value'
+      });
+
+      expect(ret)
+        .to.be.a('Promise');
+
+      return expect(ret)
+        .to.eventually.be.fulfilled;
+    });
+
+    it('should post the session context to DynamoDB', function() {
+      return expect(
+          this.sessions.write('SESSION_ID', {
+            key: 'value'
+          })
+        )
+        .to.eventually.be.fulfilled.then(() => {
+          return expect(this.db.prototype.put)
+            .to.have.been.calledWith(
+              sinon.match({
+                TableName: sinon.match.string,
+                Item: {
+                  id: 'SESSION_ID',
+                  context: {
+                    key: 'value'
+                  }
+                },
+              }),
+              sinon.match.func
+            );
         });
     });
 
-    describe('#write()', function() {
-        before(function() {
-            this.db.prototype.put = sinon.stub().callsArg(1);
-        });
-
-        it('should return a Promise', function() {
-            const ret = this.sessions.write('SESSION_ID', { key: 'value' });
-
-            expect(ret).to.be.a('Promise');
-
-            return expect(ret).to.eventually.be.fulfilled;
-        });
-
-        it('should post the session context to DynamoDB', function() {
-            return expect(
-                this.sessions.write('SESSION_ID', { key: 'value' })
-            ).to.eventually.be.fulfilled.then(() => {
-                return expect(this.db.prototype.put).to.have.been.calledWith(
-                    sinon.match({
-                        TableName: sinon.match.string,
-                        Item: {
-                            id: 'SESSION_ID',
-                            context: { key: 'value' }
-                        },
-                    }),
-                    sinon.match.func
-                );
-            });
-        });
-
-        it('should resolve with context from DynamoDB', function() {
-            return expect(
-                this.sessions.write('SESSION_ID', { key: 'value' })
-            ).to.become({
-                key: 'value'
-            });
+    it('should resolve with context from DynamoDB', function() {
+      return expect(
+          this.sessions.write('SESSION_ID', {
+            key: 'value'
+          })
+        )
+        .to.become({
+          key: 'value'
         });
     });
+  });
 });
