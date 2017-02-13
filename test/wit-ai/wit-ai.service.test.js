0c10c03aece55a6a976fc817e5276ca185e7a8ba
Sebastian Hemmilä <sebastian.hemil01@gmail.com>
2017-02-13 13:00:30 +0200

    tests for set_name, set_job and set_age

--- test/wit-ai/wit-ai.service.test.js
+++ test/wit-ai/wit-ai.service.test.js
@@ -32,6 +32,80 @@ describe('Wit.ai service', function() {
         mockery.disable();
     });
 
+    describe('#set_job', function() {
+      it('returns a Promise', function() {
+        const ret = this.wit.set_job({
+          context: {},
+          entities: {},
+        });
+
+        expect(ret).to.be.a('Promise');
+        });
+
+      it('returns the job from entity job', function() {
+        const ret = this.wit.set_job({
+          context: {},
+          entities: {
+            job: [
+              { value: 'taksikuski' },
+            ],
+          },
+        });
+
+        expect(ret).to.become({ job: 'taksikuski' });
+      });
+
+      it('preserves context', function() {
+        const ret = this.wit.set_job({
+          context: { 'foo': 'bar' },
+          entities: {
+            job: [
+              { value: 'maalari' },
+            ],
+          },
+        });
+
+        return expect(ret).to.eventually
+        .deep.equal( { 'foo': 'bar', 'job': 'maalari' } );
+      });
+    });
+
+    describe('#set_age', function() {
+      it('returns a Promise', function() {
+        const ret = this.wit.set_age({
+          context: {},
+          entities: {},
+        });
+
+        expect(ret).to.be.a('Promise');
+      });
+
+      it('returns the age from entity age', function() {
+          const ret = this.wit.set_age({
+            context: {},
+            entities: [
+              { value: '66' },
+            ],
+          });
+
+          expect(ret).to.become( { age: '66' } );
+      });
+
+      it('preserves context', function() {
+        const ret = this.wit.set_age({
+          context: { 'foo': 'bar' },
+          entities: {
+            age: [
+              { value: '43' },
+            ],
+          },
+        });
+
+        return expect(ret).to.eventually
+        .deep.equal( { 'foo': 'bar', 'age': '43' } );
+      });
+    });
+
     describe('#set_name', function() {
       it('returns a Promise', function() {
         const ret = this.wit.set_name({
@@ -66,6 +140,20 @@ describe('Wit.ai service', function() {
         });
 
         expect(ret).to.become( { name: 'Jari' } );
+      });
+
+      it('preserves context', function() {
+        const ret = this.wit.set_name({
+            context: { 'foo': 'bar' },
+            entities: {
+              name: [
+                { value: 'Jari' },
+              ],
+            },
+        });
+
+        return expect(ret).to.eventually
+        .deep.equal( { 'foo': 'bar', 'name': 'Jari' } );
       });
     });
 });
