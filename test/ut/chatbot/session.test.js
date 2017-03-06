69db4fb1af7cd36bd2efdebfe0e066b0521a4e77
Henry Carlson <henry.carlson@gmail.com>
2017-03-06 11:11:42 +0200

    Add UT cases for session

--- test/ut/chatbot/session.test.js
+++ test/ut/chatbot/session.test.js
@@ -5,17 +5,69 @@ import Session from '../../../src/chatbot/session.js';
 describe('chatbot sessions', function() {
   beforeEach(function() {
     this.dialog = {
-      checkIntent: sinon.stub()
+      checkIntent: sinon.stub(),
+      getSubStateCount: sinon.stub(),
     };
     this.session = new Session(this.dialog);
+
+    this.session._start({}, '');
+    this.dialog.getSubStateCount.returns(1);
+  });
+
+  describe('#beginDialog', function() {
+    it('should push the given dialog to top of the dialog stack', function() {
+      this.session.beginDialog('test');
+      return expect(this.session._state)
+          .to.deep.equal([['', 0], ['test', 0]]);
+    });
+    it('should increment the substate of the current dialog', function() {
+      this.dialog.getSubStateCount.returns(2);
+
+      this.session.beginDialog('test');
+      return expect(this.session._state)
+          .to.deep.equal([['', 1], ['test', 0]]);
+    });
+    it('should throw an Error if the dialog is already in the dialog stack', function() {
+      this.session.beginDialog('foo');
+      this.session.beginDialog('bar');
+      return expect(this.session.beginDialog, 'foo')
+          .to.throw(Error);
+    });
+  });
+
+  describe('#endDialog', function() {
+    it('should return to the previous dialog', function() {
+      this.session.beginDialog('foo');
+      this.session.beginDialog('bar');
+
+      this.session.endDialog();
+      return expect(this.session._state)
+          .to.deep.equal([['', 0], ['foo', 0]]);
+    });
+  });
+
+  describe('#switchDialog', function() {
+    it('should switch to the given dialog', function() {
+      this.session.switchDialog('/test');
+      expect(this.session._state[this.session._state.length-1])
+          .to.deep.equal(['test', 0]);
+    });
+  });
+
+  describe('#checkIntent', function() {
+    it('should call Builder.checkIntent', function() {
+      this.dialog.checkIntent.returns('OK');
+
+      const ret = this.session.checkIntent('test_intent');
+      expect(this.dialog.checkIntent)
+          .to.have.been.calledWith('test_intent', this.session);
+      return expect(ret).to.equal('OK');
+    });
   });
 
   describe('#_start', function() {
     it('start returns the session object', function() {
-      const ret = this.session._start(
-        {},
-        ''
-      );
+      const ret = this.session._start({}, '');
       return expect(ret).to.equal(this.session);
     });
     it('should assign context to session ', function(){
@@ -56,25 +108,6 @@ describe('chatbot sessions', function() {
       const ret = this.session._finalize();
       return expect(this.session.context.state).to.deep.equal(
           '/?1/first?3/second?0');
-    });
-  });
-
-  describe('#switchDialog', function() {
-    it('should switch to the given dialog', function() {
-      this.session.switchDialog('/test');
-      expect(this.session._state[this.session._state.length-1])
-          .to.deep.equal(['test', 0]);
-    });
-  });
-
-  describe('#checkIntent', function() {
-    it('should call Builder.checkIntent', function() {
-      this.dialog.checkIntent.returns('OK');
-
-      const ret = this.session.checkIntent('test_intent');
-      expect(this.dialog.checkIntent)
-          .to.have.been.calledWith('test_intent', this.session);
-      return expect(ret).to.equal('OK');
     });
   });
 
