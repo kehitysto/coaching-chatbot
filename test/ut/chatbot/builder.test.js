6e76608e79a0a16a4094328e29af986a596641ee
Atte Keltanen <atke@cs.helsinki.fi>
2017-03-06 13:18:55 +0200

    format

--- test/ut/chatbot/builder.test.js
+++ test/ut/chatbot/builder.test.js
@@ -3,42 +3,48 @@ import sinon from 'sinon';
 import Builder from '../../../src/chatbot/builder.js';
 
 describe('Chatbot builder', function() {
-    beforeEach(function() {
-      this.builder = new Builder({});
-      this.session = {
-        getInput: sinon.stub(),
-      };
+  beforeEach(function() {
+    this.builder = new Builder({});
+    this.session = {
+      getInput: sinon.stub(),
+    };
+  });
+
+  describe('#run', function() {
+    it('should return a Promise', function() {
+      const ret = this.builder.run({}, {});
+
+      expect(ret)
+        .to.be.a('Promise');
+    });
+  });
+
+  describe('#runAction', function() {
+    it('should return a Promise', function() {
+      const ret = this.builder.runAction({}, {}, {});
+
+      expect(ret)
+        .to.be.a('Promise');
     });
 
-    describe('#run', function() {
-      it('should return a Promise', function() {
-        const ret = this.builder.run({}, {});
-
-        expect(ret).to.be.a('Promise');
-      });
-    });
-
-    describe('#runAction', function() {
-      it('should return a Promise', function() {
-        const ret = this.builder.runAction({}, {}, {});
-
-        expect(ret).to.be.a('Promise');
-      });
-
-      it('should return an Error when there is no such actionId', function() {
+    it('should return an Error when there is no such actionId',
+      function() {
         const action = 'UNDEFINED_ACTION';
         const ret = this.builder.runAction(action, {}, {});
 
-        return expect(ret).to
-        .be.rejectedWith('No such action: ' + action);
+        return expect(ret)
+          .to
+          .be.rejectedWith('No such action: ' + action);
       });
-    });
+  });
 
-    describe('#checkIntent', function() {
-      it('should return false if intent is undefined', function() {
-        const ret = this.builder.checkIntent('UNDEFINED_INTENT', this.session);
+  describe('#checkIntent', function() {
+    it('should return false if intent is undefined', function() {
+      const ret = this.builder.checkIntent('UNDEFINED_INTENT', this
+        .session);
 
-        return expect(ret).to.be.false;
-      });
+      return expect(ret)
+        .to.be.false;
     });
+  });
 });
