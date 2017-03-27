7d350156d5e10f25f89d018eda8ece73eb68248a
Atte Keltanen <atke@cs.helsinki.fi>
2017-03-27 15:45:52 +0300

    send an educational message back to the user if the message is of unhandled type

--- src/facebook-messenger/messenger-service.js
+++ src/facebook-messenger/messenger-service.js
@@ -1,16 +1,16 @@
-/**
-* Facebook Messenger service
-*/
-
 import request from 'request-promise';
 
 import log from '../lib/logger-service';
 
 const Messenger = {
-  send(id, text, quickReplies=[]) {
+  send(id, text, quickReplies = []) {
     let body = {
-      recipient: { id },
-      message: { text },
+      recipient: {
+        id,
+      },
+      message: {
+        text,
+      },
     };
 
     if (quickReplies.length) {
@@ -27,10 +27,12 @@ const Messenger = {
     return _fbMessageRequest(body);
   },
 
-  toggleTypingIndicator(id, typing=true) {
+  toggleTypingIndicator(id, typing = true) {
     const action = (typing) ? 'typing_on' : 'typing_off';
     const body = {
-      'recipient': { id },
+      'recipient': {
+        id,
+      },
       'sender_action': action,
     };
 
@@ -42,9 +44,9 @@ const Messenger = {
       const promises = [];
 
       data.entry.forEach((entry) => {
-        entry.messaging.forEach((msgEvent) => {
-          if (msgEvent.message && !msgEvent.message.is_echo) {
-            promises.push(_receiveMessage(msgEvent, chatbot));
+        entry.messaging.forEach((messageEvent) => {
+          if (messageEvent.message && !messageEvent.message.is_echo) {
+            promises.push(_receiveMessage(messageEvent, chatbot));
           }
         });
       });
@@ -57,7 +59,9 @@ const Messenger = {
 
   verify(token, challenge) {
     if (token === process.env.FACEBOOK_VERIFY_TOKEN) {
-      return Promise.resolve({ response: challenge });
+      return Promise.resolve({
+        response: challenge,
+      });
     }
 
     return Promise.reject(new Error('400 Bad Token'));
@@ -65,44 +69,48 @@ const Messenger = {
 };
 module.exports = Messenger;
 
-
-function _receiveMessage(msgEvent, chatbot) {
+function _receiveMessage(messageEvent, chatbot) {
   return new Promise((resolve, reject) => {
-    const sender = msgEvent.sender.id;
+    const sender = messageEvent.sender.id;
+    const text = messageEvent.message.text;
 
-    const { text, attachments } = msgEvent.message;
-
-    if (attachments) {
-      return reject(new Error('Attachments are not supported'));
-    } else if (text) {
-      return resolve(
-        chatbot.receive(sender, text).then((response) => {
-          let promise = Messenger.toggleTypingIndicator(sender, true);
-          for (let i = 0; i < response.length; ++i) {
-            promise = promise.then(() => {
-              return Messenger.send(sender,
-                  response[i].message, response[i].quickReplies);
-            });
-          }
-          return promise
-              .then(() => Messenger.toggleTypingIndicator(sender, false));
-        })
-      );
+    if (!text) {
+      const unhandledMessageTypeResponse =
+        'Voit lähettää minulle vain tekstiä tai painaa antamiani nappeja.';
+      return resolve(Messenger.send(sender, unhandledMessageTypeResponse));
     }
+
+    return resolve(
+      chatbot.receive(sender, text)
+      .then((response) => {
+        let promise = Messenger.toggleTypingIndicator(sender, true);
+
+        for (let r of response) {
+          promise = promise.then(() => {
+            return Messenger.send(sender,
+              r.message, r.quickReplies);
+          });
+        }
+        return promise
+          .then(() => Messenger.toggleTypingIndicator(sender, false));
+      })
+    );
   });
 }
 
 function _fbMessageRequest(json) {
   if (!process.env.FACEBOOK_PAGE_ACCESS_TOKEN) {
     return Promise
-        .reject(new Error('No FACEBOOK_PAGE_ACCESS_TOKEN defined'));
+      .reject(new Error('No FACEBOOK_PAGE_ACCESS_TOKEN defined'));
   }
 
   log.info('Sending message to Facebook: {0}', JSON.stringify(json));
 
   return request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN },
+    qs: {
+      access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
+    },
     method: 'POST',
     json,
   });
