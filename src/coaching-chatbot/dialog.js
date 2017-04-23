145e0b377ee6ba83f6a7cfd8f6739ed896b1caa0
Henry Carlson <henry.carlson@gmail.com>
2017-04-23 22:13:15 +0300

    Display indicator when user has pending pair requests

--- src/coaching-chatbot/dialog.js
+++ src/coaching-chatbot/dialog.js
@@ -287,6 +287,11 @@ bot
         session.next();
       },
       (session) => {
+        if (session.context.pairRequests &&
+            session.context.pairRequests.length > 0) {
+          session.addResult('@TELL_USER_HAS_NEW_REQUEST');
+        }
+
         if (session.context.availablePeers.length <= 0) {
           return session.addResult('@NO_PAIRS_AVAILABLE');
         }
