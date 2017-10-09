e779209612db4d4a0d069a4e8c3c4e62482f541c
leppaott <leppanen.otto@gmail.com>
2017-10-09 15:11:30 +0300

    tests for feedback inmemory

--- test/ut/util/feedback-inmemory-provider-tests.js
+++ test/ut/util/feedback-inmemory-provider-tests.js
@@ -1,8 +1,61 @@
 import * as sinon from 'sinon';
 
-import * as Pairs from '../../../src/util/pairs-inmemory-provider';
+import * as Feedback from '../../../src/util/feedback-inmemory-provider';
 
 describe('Inmemory database service', function() {
+    before(function() {
+        this.feedback = new Feedback();
+    });
 
+    describe('#load()', function() {
+        it('should load provided data to the database', function() {
+            const expected = {
+                data: 'data',
+            };
 
+            this.feedback.load(expected);
+
+            return expect(this.feedback.db)
+                .to.deep.equal(expected);
+        });
+    });
+
+    describe('#dump()', function() {
+        it('should dump data from the database', function() {
+            const expected = {
+                data: 'data',
+            };
+
+            this.feedback.load(expected);
+
+            return expect(this.feedback.dump())
+                .to.deep.equal(expected);
+        });
+    });
+
+    describe('#write()', function() {
+        before(function() {
+            this.feedback.load([]);
+        });
+
+        it('should store data into the database', function() {
+            const expected = [{
+                date: 2323,
+                giver: 'asd',
+                pair: 23,
+                feedback: 'test',
+            }, {
+                date: 2323,
+                giver: 2,
+                pair: 3,
+                feedback: 'feedback',
+            },];
+
+            this.feedback.write(expected[0]);
+            this.feedback.write(expected[1]);
+
+            return expect(this.feedback.dump())
+                .to.deep.equal(expected);
+        });
+    });
 });
