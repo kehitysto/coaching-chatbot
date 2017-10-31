3efdc69dafd87d3b75e74760dcd1788e1ec4e653
Veini Tuominen <veini.tuominen@hotmail.com>
2017-10-31 15:21:07 +0200

    Improved flowchart generation Added support for messages without user initation. Sample discussions were expanded to include breaking pair and setting a meeting.

--- scripts/visualize.js
+++ scripts/visualize.js
@@ -2,9 +2,13 @@ import * as Chatbot from '../src/chatbot/chatbot-service';
 import * as fs from 'fs';
 import * as discussions from '../doc/flow/discussions.json';
 import dialog from '../src/coaching-chatbot/dialog';
+import * as Messenger from '../src/facebook-messenger/messenger-service';
 import * as Pairs from '../src/util/pairs-service';
 import * as path from 'path';
 import * as Sessions from '../src/util/sessions-service';
+import * as sinon from 'sinon';
+
+const messengerSpy = sinon.spy(Messenger, "send");
 
 process.env.RUN_ENV = 'dev';
 
@@ -18,31 +22,61 @@ sessions.db.load({});
 let lines = [];
 let promise = bot.receive('', '');
 
+let messageQueue = {};
+
+function addBotReply(output) {
+  for (let botReply of output) {
+    lines.push('| ' + botReply.message.replace('\n', ' | |\n| ') + ' | |');
+    if (botReply.quickReplies.length > 0) {
+      lines.push('| [' +
+        botReply.quickReplies
+          .map((quickReply) => quickReply.title)
+          .join('] [') + '] | |');
+    }
+  }
+}
+
 for (let scenario in discussions) {
   const messages = discussions[scenario].messages;
 
-  promise.then(() => {
+  promise = promise.then(() => {
     lines.push('# ' + scenario);
     lines.push('| Kehitystön vertaisohjausrobotti | ' + discussions[scenario].player + ' |');
     lines.push('|-|-|');
   });
 
+  let sessionID = discussions[scenario].session;
+
+  promise = promise.then(() => {
+    if (messageQueue[sessionID] != undefined) {
+      addBotReply(messageQueue[sessionID]);
+      messageQueue[sessionID] = undefined;
+    }
+  });
+
   for (let message of messages) {
     promise = promise.then(() => {
       lines.push('| | ' + message + ' |');
-      return bot.receive(discussions[scenario].session, message);
+      return bot.receive(sessionID, message);
     }).then((output) => {
-      for (let botReply of output) {
-        lines.push('| ' + botReply.message.replace('\n', ' | |\n| ') + ' | |');
-        if (botReply.quickReplies.length > 0) {
-          lines.push('| [' +
-            botReply.quickReplies
-              .map((quickReply) => quickReply.title)
-              .join('] [') + '] | |');
-        }
-      }
+      addBotReply(output);
     });
   }
+
+  promise = promise.then(() => {
+    if (messengerSpy.callCount > 0) {
+      for (let call of messengerSpy.args) {
+        if (!messageQueue[call[0]]) {
+          messageQueue[call[0]] = [];
+        }
+        messageQueue[call[0]].push({
+          message: call[1],
+          quickReplies: call[2] || {}
+        });
+      }
+      messengerSpy.reset();
+    }
+  });
 }
 
 promise = promise.then(() => {
