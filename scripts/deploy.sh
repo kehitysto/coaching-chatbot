b8a785f8121472c302eaf7207eb4bfc9f7b2684c
Samuli Rouvinen <samrouvi@dhcp-asv-112.helsinki.fi>
2017-11-06 11:04:46 +0200

    Testing deploy changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,9 @@ STAGE=$1
 npm run write-env
 npm i -g serverless
 
+if [ $TRAVIS_BRANCH = "feature/add-production-deployment" ]
+then
+  echo 'JEEEEEI'
+  if [ -z ${PRODUCTION_PAGE_ACCESS_TOKEN+x} ]; then echo "var is unset"; else echo "var is set to '$PRODUCTION_PAGE_ACCESS_TOKEN'"; fi
+fi
 serverless deploy --stage=$STAGE
