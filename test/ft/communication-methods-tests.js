eb0b8076b9844197799d6d7076e469f8cd4b7244
Samuli Rouvinen <samrouvi@dhcp-asv-112.helsinki.fi>
2017-11-07 12:35:25 +0200

    Added feature tests for removing and editing communication methods

--- test/ft/communication-methods-tests.js
+++ test/ft/communication-methods-tests.js
@@ -1,6 +1,6 @@
 import commonFeatures from './common';
-import CommunicationMethodsFormatter from '../../src/lib/communication-methods-formatter';
 import PersonalInformationFormatter from '../../src/lib/personal-information-formatter-service';
+import CommunicationMethodsFormatter from '../../src/lib/communication-methods-formatter';
 const { buildResponse, FeatureTestStates, setupChatbot, QuickReplies } = commonFeatures;
 
 const SESSION = 'COMMUNICATION_METHODS_TESTER';
@@ -101,6 +101,52 @@ describe('Communication methods tests', function() {
       );
 
       it(
+        'should go to removing when user wants it to',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'Poista'))
+            .to.eventually.become([
+              buildResponse('@REQUEST_COMMUNICATION_METHOD_DELETE', [{
+                'title': 'Skype',
+                'payload': 'SKYPE',
+              }, {
+                'title': 'Puhelin',
+                'payload': 'PHONE',
+              }, {
+                'title': 'Palaa',
+                'payload': '@RETURN',
+              }]),
+            ]);
+        }
+      );
+
+      it(
+        'should go back to asking when a communication method is removed',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'SKYPE'))
+            .to.eventually.become([
+              buildResponse(PersonalInformationFormatter.formatFromTemplate(
+                '@CONFIRM_COMMUNICATION_METHODS', {
+                  communicationMethods: {
+                    PHONE: '040404'
+                  }
+                })),
+              buildResponse('@PROVIDE_OTHER_COMMUNICATION_METHODS', [{
+                'title': 'Lisää',
+                'payload': '@EDIT',
+              }, {
+                'title': 'Poista',
+                'payload': '@DELETE',
+              }, {
+                'title': 'Profiiliin',
+                'payload': '@TO_PROFILE',
+              }]),
+            ]);
+        }
+      );
+
+      it(
         'should go to profile',
         function() {
           return expect(
@@ -111,6 +157,172 @@ describe('Communication methods tests', function() {
                   '@DISPLAY_PROFILE', FeatureTestStates['COMMUNICATION_METHODS_TESTS']['sessions'][SESSION]),
                 PersonalInformationFormatter.getPersonalInformationbuttons(
                   {})),
+            ]);
+        }
+      );
+    }
+  );
+
+  describe(
+    'As a registered user I want to be able to remove my communication methods, add them again and modify them',
+    function() {
+      before(function() {
+        setupChatbot(this, 'COMMUNICATION_METHODS_REMOVAL_TESTS');
+      });
+
+      it(
+        'should ask for my Skype username when I choose Skype as a communication method',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'Skype'))
+            .to.eventually.become([
+              buildResponse('@REQUEST_SKYPE_NAME'),
+            ]);
+        }
+      );
+
+      it(
+        'should ask if the user wants to add more methods',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'nickname'))
+            .to.eventually.become([
+              buildResponse(PersonalInformationFormatter.formatFromTemplate(
+                '@CONFIRM_COMMUNICATION_METHODS', {
+                  communicationMethods: {
+                    SKYPE: 'nickname'
+                  }
+                })),
+              buildResponse('@PROVIDE_OTHER_COMMUNICATION_METHODS', [{
+                'title': 'Lisää',
+                'payload': '@EDIT',
+              }, {
+                'title': 'Poista',
+                'payload': '@DELETE',
+              }, {
+                'title': 'Profiiliin',
+                'payload': '@TO_PROFILE',
+              }]),
+            ]);
+        }
+      );
+
+      it(
+        'should allow removing the only communication method',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'Poista'))
+            .to.eventually.become([
+              buildResponse('@REQUEST_COMMUNICATION_METHOD_DELETE', [{
+                'title': 'Skype',
+                'payload': 'SKYPE',
+              }, {
+                'title': 'Palaa',
+                'payload': '@RETURN',
+              }]),
+            ]);
+        }
+      );
+
+      it(
+        'should ask for methods without letting the user go to profile',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'SKYPE'))
+            .to.eventually.become([
+              buildResponse('@REQUEST_COMMUNICATION_METHOD', [{
+                'title': 'Skype',
+                'payload': 'SKYPE',
+              }, {
+                'title': 'Puhelin',
+                'payload': 'PHONE',
+              }]),
+            ]);
+        }
+      );
+
+      it(
+        'should allow adding already once removed methods',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'Skype'))
+            .to.eventually.become([
+              buildResponse('@REQUEST_SKYPE_NAME'),
+            ]);
+        }
+      );
+
+      it(
+        'should ask if the user wants to add more methods',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'nickname'))
+            .to.eventually.become([
+              buildResponse(PersonalInformationFormatter.formatFromTemplate(
+                '@CONFIRM_COMMUNICATION_METHODS', {
+                  communicationMethods: {
+                    SKYPE: 'nickname'
+                  }
+                })),
+              buildResponse('@PROVIDE_OTHER_COMMUNICATION_METHODS', [{
+                'title': 'Lisää',
+                'payload': '@EDIT',
+              }, {
+                'title': 'Poista',
+                'payload': '@DELETE',
+              }, {
+                'title': 'Profiiliin',
+                'payload': '@TO_PROFILE',
+              }]),
+            ]);
+        }
+      );
+
+      it('should allow adding methods after removal',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'Lisää'))
+            .to.eventually.become([
+              buildResponse('@REQUEST_COMMUNICATION_METHOD',
+                CommunicationMethodsFormatter
+                .getCommunicationMethods({})),
+            ]);
+        }
+      );
+
+      it(
+        'should allow editing methods already provided',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'Skype'))
+            .to.eventually.become([
+              buildResponse('@REQUEST_SKYPE_NAME'),
+            ]);
+        }
+      );
+
+      it(
+        'should ask if the user wants to add more methods',
+        function() {
+          return expect(
+              this.bot.receive(SESSION, 'someothernickname'))
+            .to.eventually.become([
+              buildResponse(PersonalInformationFormatter.formatFromTemplate(
+                '@CONFIRM_COMMUNICATION_METHODS', {
+                  communicationMethods: {
+                    SKYPE: 'someothernickname'
+                  }
+                })),
+              buildResponse('@PROVIDE_OTHER_COMMUNICATION_METHODS', [{
+                'title': 'Lisää',
+                'payload': '@EDIT',
+              }, {
+                'title': 'Poista',
+                'payload': '@DELETE',
+              }, {
+                'title': 'Profiiliin',
+                'payload': '@TO_PROFILE',
+              }]),
             ]);
         }
       );
