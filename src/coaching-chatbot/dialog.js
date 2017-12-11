d7d7af67be68b60dfa8b244ca66080dbd6538004
pullari <samuli.rouvinen@gmail.com>
2017-12-11 12:40:43 +0200

    Sending request message separately

--- src/coaching-chatbot/dialog.js
+++ src/coaching-chatbot/dialog.js
@@ -437,7 +437,7 @@ bot
       (session) => {
         session.addResult('@REQUEST_LIST_LENGTH');
         session.addResult('@INFORMATION_ABOUT_REQUESTS');
-        session.runActions(['displayRequest']);
+        session.runActions(['displayRequest', 'displayRequestMessage']);
         session.addQuickReplies(
           Builder.QuickReplies.createArray(['@YES', '@NO', '@RETURN'])
         );
